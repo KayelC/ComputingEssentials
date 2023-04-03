08d0a82f4031037f5265aea415a12af173902f0a
Kayel Calleja <129683809+KayelC@users.noreply.github.com>
2023-04-03 16:05:52 +0200

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"SignMagnitudeIntegers"},{"p":"<Unnamed>","l":"SignMagnitudeIntegers2"},{"p":"<Unnamed>","l":"UnsignedIntegers"}];updateSearchResults();
